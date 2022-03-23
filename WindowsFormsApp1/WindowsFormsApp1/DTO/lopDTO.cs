9ce0a89e382fa773670a530dcb08d6d01086bb65
THẮNG ĐẶNG <dangthangtn123@gmail.com>
2022-03-23 15:44:33 +0700

    new

--- WindowsFormsApp1/WindowsFormsApp1/DTO/lopDTO.cs
+++ WindowsFormsApp1/WindowsFormsApp1/DTO/lopDTO.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace WindowsFormsApp1.DTO
 {
-    class lopDTO
+    public class lopDTO
     {
+        private string _MaLop;
+        private string _TenLop;
+        private string _HDT;
+        private string _Siso;
+
+        public string MaLop { get => _MaLop; set => _MaLop = value; }
+        public string TenLop { get => _TenLop; set => _TenLop = value; }
+        public string HDT { get => _HDT; set => _HDT = value; }
+        public string Siso { get => _Siso; set => _Siso = value; }
     }
 }
